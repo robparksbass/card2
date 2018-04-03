f22acbf995712892c0ad88d9956895c5c18b112f
Parks <robparks@Rob-Parks.local>
2018-04-03 14:17:21 -0400

    changes?

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 <nav class="row">
     <div class="col-12">
         <ul class="mainNav">
-            <li><a href="cards" class="btn btn-dark">Cards</a></li>
+            <li><a href="cards" class="btn btn-dark">CarDFDFds</a></li>
             <li><a href="businesses" class="btn btn-dark">Businesses</a></li>
             <li><a href="about" class="btn btn-primary">About</a></li>
             <li><a href="add"><img src="img/add-btn.png" alt="Add a new Card" class="addBtn"></a></li>
