0629f133f8188499e3a1df8759dd777ee23e71ff
Parks <robparks@Rob-Parks.local>
2018-04-03 15:44:59 -0400

    stilltesting

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <div class="col-12">
         <ul class="mainNav">
             <li><a href="cards" class="btn btn-dark">Cards</a></li>
-            <li><a href="businesses" class="btn btn-dark">Busidfdfdfnesses</a></li>
+            <li><a href="businesses" class="btn btn-dark">Businesses</a></li>
             <li><a href="about" class="btn btn-primary">About</a></li>
             <li><a href="add"><img src="img/add-btn.png" alt="Add a new Card" class="addBtn"></a></li>
         </ul>
