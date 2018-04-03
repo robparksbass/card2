099c77eca231be326b095b8b83dea64c8b48d460
Parks <robparks@Rob-Parks.local>
2018-04-03 17:21:24 -0400

    users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->integer('user_level');
+            $table->integer('user_level')->default(1);;
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -7,13 +7,13 @@
     
     <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="">Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="login">Login</a>
         </li>
         <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+        <a class="nav-link" href="register">Register</a>
         </li>
     </ul>
 
