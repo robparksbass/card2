d44a5849d004cc89f0e46e74634acd9f0dc67221
Rob Parks <robparksbass@gmail.com>
2018-04-05 11:13:15 -0400

    changes from seans suggestions on businesses page

--- resources/views/businesses/index.blade.php
+++ resources/views/businesses/index.blade.php
@@ -12,7 +12,12 @@
                     </li>
                 @endforeach
             </ul>
+            @guest
+            Create an account or login to access the full features.<br>
+            <a href="/">Back to Home</a>
+            @else
             <a href="home">Back to Portal</a>
+            @endguest
         </div>
     </main>
 @endsection
