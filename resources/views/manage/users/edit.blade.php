592906f19766f2f6592b5bfb0e8bd8beb9230a97
Rob Parks <robparksbass@gmail.com>
2018-04-27 17:00:45 -0400

    resolved edit dirty write

--- resources/views/manage/users/edit.blade.php
+++ resources/views/manage/users/edit.blade.php
@@ -1,43 +1,33 @@
 @extends('layouts.app')
-
 @section('content')
-    <div class="flex-container">
-        <div class="columns m-t-10">
-            <div class="column">
-                <h1 class="title">Edit User</h1>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Edit User Details</div>
+                <div class="card-body">
+                    <form action="{{ route('users.update', $user->id) }}" method="POST">
+                        {{ method_field('PUT') }}
+                        {{ csrf_field() }}
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
+                            <div class="col-md-6">
+                                <input type="text" class="input" name="name" id="name" value="{{ $user->name }}">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
+                            <div class="col-md-6">
+                                <input type="text" class="input" name="email" id="email" value="{{ $user->email }}">
+                            </div>
+                        </div>                    
+                        <div class="row justify-content-center">
+                            <button class="btn btn-success btn-sm">Edit User</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-    <hr class="m-t-0">
-
-    <div class="columns">
-        <div class="column">
-            <form action="{{ route('users.update', $user->id) }}" method="POST">
-            {{ csrf_field() }}
-                <div class="field">
-                    <label for="name" class="label">Name</label>
-                    <input type="text" class="input" name="name" id="name">
-                </div>
-                <div class="field">
-                    <label for="email" class="label">Email:</label>
-                    <p class="control">
-                        <input type="text" class="input" name="email" id="email">
-                    </p>
-                </div>
-                <div class="field">
-                    <label for="password" class="label">Password:</label>
-                    <input type="radio" name="keep" value="keep">Keep
-                    <input type="radio" name="manual" value="manual">Manual
-                    <input type="radio" name="auto" value="auto">Auto
-                    <p class="control">
-                        <input type="text" class="input" name="password" id="password" placeholder="Manually give a password to this user">
-                    </p>
-                    <!-- <p class="control">
-                        <input type="text" class="input" name="password" id="password" v-if="password-options == 'manual'" placeholder="Manually give a password to this user">
-                    </p> -->
-                </div>
-                <button class="button is-primary ">Edit User</button>
-            </form>
-        </div>
-    </div>
+    </div>               
+</div>            
 @endsection
