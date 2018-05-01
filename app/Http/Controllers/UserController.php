78d02a5d7ade8cf1329898a420b9bcb061b18c1f
Rob Parks <robparksbass@gmail.com>
2018-05-01 10:43:49 -0400

    fixing verification

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,15 +6,13 @@ use Illuminate\Http\Request;
 use App\User;
 use Session;
 use Hash;
-use Illuminate\Validation\Rule;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\View\Middleware\ShareErrorsFromSession;
-
+// use Illuminate\Validation\Rule;
+// use Illuminate\Support\Facades\Auth;
+// use Illuminate\Support\Facades\Validator;
+// use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 class UserController extends Controller
 {
-
     /**
      * Display a listing of the resource.
      *
@@ -52,10 +50,12 @@ class UserController extends Controller
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed'
         ]);
+
         $user = new User();
         $user->name = $request->name;
         $user->email = $request->email;
         $user->password = Hash::make(trim($request->password));
+        // $user->save();
 
         if($user->save()) {
             return redirect()->route('users.show', $user->id);
@@ -98,21 +98,23 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $user = User::findOrFail($id);
-
         $this->validate($request, [
             'name' => 'required|string|max:255',
-            'email' => 'required|email|max:255|unique:users',  
-            'email' => Rule::unique('users')->ignore($user->id),
+            'email' => 'required|email',  Rule::unique('users')->ignore($id),
         ]);
-     
+
+        $user = User::findOrFail($id);
         $user->name = $request->name;
         $user->email = $request->email;
-        $saved = $user->save();
 
         if($user->save()) {
             return redirect()->route('users.show', $id);
+        } else {
+            Session::flash('error', 'There was a problem saving the updated user info. Please try again.');
+            return redirect()->route('users.edit', $id);
         }
+        
+
     }
 
     /**
